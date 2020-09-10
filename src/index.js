07afac14fd61b23e5886f5aa17576e425f56e580
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-10 11:09:04 -0600

    Added a working component for hello

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './components/Hello';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
