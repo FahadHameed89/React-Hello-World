77de079297a05d815c3bd14fac215e9b83c19e6e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-10 11:33:10 -0600

    Added an example on how to input and display PROPs

--- src/components/HelloStudent.js
+++ src/components/HelloStudent.js
@@ -3,10 +3,10 @@ import React from 'react';
 // WE can use fat arrow syntax to define components as well.
 // If the component accepts a parementer, this is referred to as a 'prop' . In this case, 'name' is a prop
 // Props alllow us to pass values into our components from a parent document / component. 
-const HelloStudent = ( name ) => {
+const HelloStudent = ( props ) => {
     return (
         <p>
-            Hello there { name }! We come in peace, don't try to escape.
+            Hello there { props.name }! We come in peace, don't try to escape. You are { props.age } old, don't ask me how I kow...
         </p>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './components/Hello';
+import HelloStudent from './components/HelloStudent';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
-    <Hello />
+    <HelloStudent name="Fahad" age="30"/>
 
   </React.StrictMode>,
   document.getElementById('root')
