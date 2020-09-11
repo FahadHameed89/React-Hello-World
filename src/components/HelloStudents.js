f6cf6b95890861ca336149a9ec743e24afee35f8
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-11 10:15:18 -0600

    Displaying the output of created list with propertiesgit add -A!

--- src/components/HelloStudents.js
+++ src/components/HelloStudents.js
@@ -3,12 +3,17 @@ import HelloStudent from './HelloStudent';
 
 function HelloStudents () 
 {
-    const students = [ "Fahad", "Samson", "Drake" ];
-    let counter = 0;
-    const studentsList = students.map( studentName => <HelloStudent key={counter++} name={studentName} />);    // Map is a method that creates a key value pair based on what we are returning
+    const students = [ 
+        {name:"Fahad", cost:"24MP", effect:"Iron Body"}, 
+        {name:"Samson", cost:"12MP", effect:"Sword Catcher"}, 
+        ];
+    // Create a counter starting at 0 which ramps up to assign the key. 
+        let counter = 0;
+    const studentsList = students.map( student => <HelloStudent key={counter++} name={student.name} cost={student.cost} effect={student.effect} />);    // Map is a method that creates a key value pair based on what we are returning
 
     // Grabbing name, feeding it into studentsName. For each one of these items, create a map that has Hellostudent name as the value.
 
+    // Analyze provided data, check in console log via JS
     console.log ( studentsList);
 
     return ( 
