e1f4b56ea1fa51c27112641e8d3a1e3d6e204f90
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-11 10:09:04 -0600

    Changed the key generation from equalling name to equalling counter state

--- src/components/HelloStudents.js
+++ src/components/HelloStudents.js
@@ -4,8 +4,9 @@ import HelloStudent from './HelloStudent';
 function HelloStudents () 
 {
     const students = [ "Fahad", "Samson", "Drake" ];
-    const studentsList = students.map( studentName => <HelloStudent key={studentName} name={studentName} />);    // Map is a method that creates a key value pair based on what we are returning
-                                                                                // Fat Arrow is assigning name to equal to studentName
+    let counter = 0;
+    const studentsList = students.map( studentName => <HelloStudent key={counter++} name={studentName} />);    // Map is a method that creates a key value pair based on what we are returning
+
     // Grabbing name, feeding it into studentsName. For each one of these items, create a map that has Hellostudent name as the value.
 
     console.log ( studentsList);
